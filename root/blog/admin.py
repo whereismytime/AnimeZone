d3f3bcd438077ffa26dfb123b66d2215b09e4a2c
TCG <tcg@d.co>
2025-05-16 13:31:10 +0300

    модель блогу

--- root/blog/admin.py
+++ root/blog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import BlogPost
 
-# Register your models here.
+@admin.register(BlogPost)
+class BlogPostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'status', 'created_at', 'published_at')
+    list_filter = ('status', 'author')
+    search_fields = ('title', 'content')
+    prepopulated_fields = {'slug': ('title',)}
+    date_hierarchy = 'published_at'
+    ordering = ['-published_at']
